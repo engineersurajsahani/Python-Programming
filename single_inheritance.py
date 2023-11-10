3007c175f0bb6515669188ce957114b97d94d0c3
Suraj Sahani <93707688+surajsahani9321@users.noreply.github.com>
2023-11-10 22:02:09 +0530

    10/11/2023

---
+++ single_inheritance.py
@@ -0,0 +1,16 @@
+class A:
+    
+    def display(slef):
+        print("Class A")
+    
+class B(A):
+    
+    def display(slef):
+        print("Class B")
+
+        
+a=A()
+a.display()
+
+b=B()
+b.display()
