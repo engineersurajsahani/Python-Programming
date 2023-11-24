c14b378495188903e3607f6a486df5da98001d93
Suraj Sahani <93707688+surajsahani9321@users.noreply.github.com>
2023-11-24 11:41:18 +0530

    24/11/2023

---
+++ file_write.py
@@ -0,0 +1,8 @@
+# File Write
+
+f=open("surajsahani.txt","wt")
+f.write("\nI am a Suraj Sahani")
+f.write("\nI am a Full Stack Web Developer")
+f.write("\nI am a Full Stack Java Developer")
+f.write("\nI am a Full Stack Python Developer")
+f.close()
