a973ded0f54bcb4c77fa9f5832c77e15143c60d0
Suraj Sahani <93707688+surajsahani9321@users.noreply.github.com>
2024-01-14 06:32:32 +0530

    14/01/2024

---
+++ lambda_function.py
@@ -0,0 +1,17 @@
+# Lambda Function
+
+add=lambda a,b : a+b
+
+sub=lambda a,b : a-b
+
+mul=lambda a,b : a*b 
+
+div=lambda a,b : a/b 
+
+print("Addition :- ",add(5,6))
+
+print("Subraction :- ",sub(77,11))
+
+print("Multiplication :- ",mul(4,5))
+
+print("Division :- ",div(66,11))
